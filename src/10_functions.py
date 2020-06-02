915d1a1403c241b6882730f60e00c755a8ff3c94
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-06-02 14:58:19 -0700

    got that issue resolved with the 10_funtions part

--- src/10_functions.py
+++ src/10_functions.py
@@ -16,8 +16,8 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
-if is_even(num)
+if is_even(num):
   print("Even!")
-    else:
-      print("Odd!")
+else:
+  print("Odd!")
 
